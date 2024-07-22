c3ab37831207523e87e4f4fa85e604b48e1ca954
AlaaAlzahrani <alzahrani.alaaa@gmail.com>
2024-07-22 18:55:49 +0300

    update innit file

--- __init__.py
+++ __init__.py
@@ -1,9 +1,2 @@
-# ARSA/__init__.py
+# This file is intentionally left empty
 
-from .get_analysis import automatic_analyzer
-
-__all__ = ['automatic_analyzer']
-
-__version__ = '0.1.0'
-__author__ = 'Alaa Alzahrani'
-__description__ = 'Arabic syntactic analyzer'
